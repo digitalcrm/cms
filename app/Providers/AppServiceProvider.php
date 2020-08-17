5c856a759fa487b3206282df23dd73464fe77fdd
Neeraj1005 <neeraj.tangariya123@gmail.com>
2020-08-17 17:11:54 +0530

    scaffold and default role assigned added

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\User;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        User::created(function ($user){
+            $user->assignRole(3);
+        });
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @hasrole('superadmin')
+                    {{ __('You are logged in as SuperAdmin!') }}
+                    @else
+                    {{ __('You are logged in as user!') }}
+                    @endhasrole
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
