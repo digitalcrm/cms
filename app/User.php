a855520eafdea5bc5d735f2607a18e6ff6d6d425
Neeraj1005 <neeraj.tangariya123@gmail.com>
2020-09-07 20:10:33 +0530

    verify email added

--- app/User.php
+++ app/User.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable, HasRoles, SoftDeletes;
 
--- routes/web.php
+++ routes/web.php
@@ -7,41 +7,45 @@
         return view('welcome');
     });
 
-    Auth::routes();
+    Auth::routes(['verify' => true]);
 
     // Route::get('/home', 'HomeController@index')->name('home');
 
-    Route::group(['namespace' => 'SuperAdmin'], function () {
+    Route::group(['middleware' => ['verified']], function () {
 
-        Route::get('/dashboard','SuperAdminController@index')->name('dashboard');
+        Route::group(['namespace' => 'SuperAdmin'], function () {
 
-        Route::group(['middleware' => ['role:superadmin|admin']], function () {
-            # user create only by superADMIN and admin
+            Route::get('/dashboard','SuperAdminController@index')->name('dashboard');
 
-            Route::get('auth/create', 'SuperAdminController@create')->name('auth.create')->middleware('permission:user-create');
+            Route::group(['middleware' => ['role:superadmin|admin']], function () {
+                # user create only by superADMIN and admin
 
-            Route::post('auth/create', 'SuperAdminController@store')->name('auth.store')->middleware('permission:user-create');
+                Route::get('auth/create', 'SuperAdminController@create')->name('auth.create')->middleware('permission:user-create');
 
-            Route::get('/all-users','SuperAdminController@getAllUsers')->name('all-users')->middleware('permission:all-users');
+                Route::post('auth/create', 'SuperAdminController@store')->name('auth.store')->middleware('permission:user-create');
+
+                Route::get('/all-users','SuperAdminController@getAllUsers')->name('all-users')->middleware('permission:all-users');
+            });
+        });
+
+        Route::group(['namespace' => 'Posts'], function () {
+            Route::resource('posts', 'PostController');
+            Route::put('posts/isactive/{isActive}','PostController@isActive')->name('posts.status');
+        });
+
+        Route::group(['namespace' => 'Category'], function () {
+            Route::resource('category', 'CategoryController');
+
+            Route::resource('subcategory', 'SubCategoryController');
+        });
+
+        Route::group(['namespace' => 'Tag'], function () {
+            Route::resource('tag', 'TagController');
+        });
+
+        Route::group(['namespace' => 'RoleManagement'], function () {
+            Route::resource('permission', 'PermissionController');
+            Route::resource('role', 'RoleController');
         });
     });
 
-    Route::group(['namespace' => 'Posts'], function () {
-        Route::resource('posts', 'PostController');
-        Route::put('posts/isactive/{isActive}','PostController@isActive')->name('posts.status');
-    });
-
-    Route::group(['namespace' => 'Category'], function () {
-        Route::resource('category', 'CategoryController');
-
-        Route::resource('subcategory', 'SubCategoryController');
-    });
-
-    Route::group(['namespace' => 'Tag'], function () {
-        Route::resource('tag', 'TagController');
-    });
-
-    Route::group(['namespace' => 'RoleManagement'], function () {
-        Route::resource('permission', 'PermissionController');
-        Route::resource('role', 'RoleController');
-    });
