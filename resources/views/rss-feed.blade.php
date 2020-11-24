7254e4e1c813f8a68787db0a8194cdaef9e695fa
Neeraj1005 <neeraj.tangariya123@gmail.com>
2020-11-24 17:02:13 +0530

    rss feed added

--- resources/views/rss-feed.blade.php
+++ resources/views/rss-feed.blade.php
@@ -1,39 +1,31 @@
 <rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 
-<channel>
+    <channel>
 
-  <atom:link type="application/rss+xml" rel="self" href="{{ $sites['feeds']['main']['url'] }}"/>
+        <atom:link type="application/rss+xml" rel="self" href="{{ $sites['feeds']['main']['url'] }}"/>
+        <title>{{ $sites['feeds']['main']['title'] }}></title>
+        <link>{{ env('APP_URL') }}</link>
+        <description>{{ $sites['feeds']['main']['description'] }}</description>
+        <copyright>{{ $sites['feeds']['main']['copyright'] }}</copyright>
+        <language>{{ $sites['feeds']['main']['language'] }}</language>
+        <lastBuildDate>{{ $rss_latest_posts->first()->updated_at->toRssString() }}</lastBuildDate>
+        <!--<image>
+            <title>{{ $sites['feeds']['main']['title'] }}</title>
+            <url>{{ $sites['feeds']['main']['logo_url'] }}</url>
+            <link>{{ env('APP_URL') }}</link>
+        </image>-->
 
-  <title>{{ $sites['feeds']['main']['title'] }}></title>
-
-  <link>{{ env('APP_URL') }}</link>
-
-  <description>{{ $sites['feeds']['main']['description'] }}</description>
-
-  <copyright>{{ $sites['feeds']['main']['copyright'] }}</copyright>
-
-  <language>{{ $sites['feeds']['main']['language'] }}</language>
-
-  <lastBuildDate>{{ $rss_latest_posts->first()->updated_at->toRssString() }}</lastBuildDate>
-
-  <image>
-    <title>{{ $sites['feeds']['main']['title'] }}</title>
-    <url>{{ $sites['feeds']['main']['logo_url'] }}</url>
-    <link>{{ env('APP_URL') }}</link>
-  </image>
-
-    @foreach($rss_latest_posts as $post)
-    <item>
-      <title>RSS Tutorial</title>
-      <description></description>
-      <link>{{ route('post.viewitem',$post->slug) }}</link>
-      <guid>{{ route('post.viewitem',$post->slug) }}</guid>
-      <description><![CDATA[{!! $post->summary_of_body !!}]]></description>
-      <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
-    </item>
-    @endforeach
+        @foreach($rss_latest_posts as $post)
+        <item>
+            <title>{{ $post->title }}</title>
+            <description><![CDATA[{!! $post->summary_of_body !!}]]></description>
+            <link>{{ route('post.viewitem',$post->slug) }}</link>
+            <guid>{{ route('post.viewitem',$post->slug) }}</guid>
+            <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
+        </item>
+        @endforeach
 
 
-</channel>
+    </channel>
 
 </rss>
