14e73041fa989e268d62fec824503ccb9bd113d3
Neeraj1005 <neeraj.tangariya123@gmail.com>
2020-11-27 16:40:55 +0530

    livewire config file added

--- resources/views/layouts/partials/homepage-logo.blade.php
+++ resources/views/layouts/partials/homepage-logo.blade.php
@@ -1,5 +1,5 @@
 <a class="navbar-brand" href="{{ url('/') }}">
-@if(!empty($homepage_logo))
+@if(!empty($homepage_logo->logo_path))
     <img src="{{ $homepage_logo->homepageLogoURL() }}" alt="{{ $homepage_logo->altText() }}" style="height: 1em; width: 2em;">
 @else
     AYN<span>SOFT</span>
